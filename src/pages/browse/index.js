c1b8a25a56bd93bb4da42e1dfe5372cdcd9f7ce3
João Pedro Pagno <pagno.jp@gmail.com>
2019-10-09 17:49:53 -0300

    add browse

---
+++ src/pages/browse/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import {
+  Container, Title, List, Playlist,
+} from './styles';
+
+const Browse = () => (
+  <Container>
+    <Title>Browse</Title>
+    <List>
+      <Playlist to="/playlists/1">
+        <img src="https://i.scdn.co/image/55cc31d3efa495d05e034146331553db0a747cf5" alt="Playlist" />
+        <strong>One of the most successful acts in rock history</strong>
+        <p>
+          The band was inducted into the Rock and Roll Hall of Fame in April 2012.
+           The Red Hot Chili Peppers’ last studio album, The Getaway, was released by Warner Bros.
+           Records on June 17, 2016.
+        </p>
+      </Playlist>
+      <Playlist to="/playlists/1">
+        <img src="https://i.scdn.co/image/55cc31d3efa495d05e034146331553db0a747cf5" alt="Playlist" />
+        <strong>One of the most successful acts in rock history</strong>
+        <p>
+          The band was inducted into the Rock and Roll Hall of Fame in April 2012.
+           The Red Hot Chili Peppers’ last studio album, The Getaway, was released by Warner Bros.
+           Records on June 17, 2016.
+        </p>
+      </Playlist>
+      <Playlist to="/playlists/1">
+        <img src="https://i.scdn.co/image/55cc31d3efa495d05e034146331553db0a747cf5" alt="Playlist" />
+        <strong>One of the most successful acts in rock history</strong>
+        <p>
+          The band was inducted into the Rock and Roll Hall of Fame in April 2012.
+           The Red Hot Chili Peppers’ last studio album, The Getaway, was released by Warner Bros.
+           Records on June 17, 2016.
+        </p>
+      </Playlist>
+      <Playlist to="/playlists/1">
+        <img src="https://i.scdn.co/image/55cc31d3efa495d05e034146331553db0a747cf5" alt="Playlist" />
+        <strong>One of the most successful acts in rock history</strong>
+        <p>
+          The band was inducted into the Rock and Roll Hall of Fame in April 2012.
+           The Red Hot Chili Peppers’ last studio album, The Getaway, was released by Warner Bros.
+           Records on June 17, 2016.
+        </p>
+      </Playlist>
+    </List>
+  </Container>
+);
+
+export default Browse;
