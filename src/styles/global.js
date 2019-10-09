2b3df0e042c236383d3aaa6a9db171ef35fedb88
João Pedro Pagno <pagno.jp@gmail.com>
2019-10-09 10:06:47 -0300

    add GlobalStyle

---
+++ src/styles/global.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    outline: 0;
+  }
+  html, body, #root {
+    height: 100%;
+  }
+  body {
+    background-color: #181818;
+    color: #FFF;
+    font-family: 'Montserrat', sans-serif;
+    text-rendering: optimizeLegibility;
+  }
+`;
+
+export default GlobalStyle;
