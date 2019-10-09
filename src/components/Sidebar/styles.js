37eba2c0c35b8bd91444a93eae5f258124815eb0
João Pedro Pagno <pagno.jp@gmail.com>
2019-10-09 11:13:56 -0300

    add NewPlaylist styled button

--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -9,3 +9,20 @@ export const Container = styled.aside`
   flex-direction: column;
   justify-content: space-between;
 `;
+
+export const NewPlaylist = styled.button`
+  background: transparent;
+  border: 0;
+  border-top: 1px solid #282828;
+  font-size: 13px;
+  color: #b3b3b3;
+  display: flex;
+  align-items: center;
+  padding: 15px 25px;
+  &:hover {
+    color: #fff;
+  }
+  img {
+    margin-right: 10px;
+  }
+`;
