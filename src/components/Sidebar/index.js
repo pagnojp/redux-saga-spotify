523bbdae25e65e5d9998124203caf0eab84621fd
João Pedro Pagno <pagno.jp@gmail.com>
2019-10-09 11:12:41 -0300

    add styled button NewPlaylist

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, NewPlaylist } from './styles';
+import AddPlaylistIcon from '../../assets/images/add_playlist.svg';
+
+const Sidebar = () => (
+  <Container>
+    <div />
+    <NewPlaylist>
+      <img src={AddPlaylistIcon} alt="Add Playlist" />
+      New Playlist
+    </NewPlaylist>
+  </Container>
+);
+
+export default Sidebar;
