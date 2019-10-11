7d8e481fd7cc173ee12f9317eaf5a799e490c2a1
João Pedro Pagno <pagno.jp@gmail.com>
2019-10-11 14:57:28 -0300

    add ErrorBox

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import GlobalStyle from './styles/global';
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
 import Header from './components/Header';
-
+import ErrorBox from './components/ErrorBox';
 import { Wrapper, Container, Content } from './styles/components';
 
 import Routes from './routes';
@@ -22,6 +22,7 @@ function App() {
             <Container>
               <Sidebar />
               <Content>
+                <ErrorBox />
                 <Header />
                 <Routes />
               </Content>
