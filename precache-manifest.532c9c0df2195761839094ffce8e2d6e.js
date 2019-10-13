0cabbe2cd491bb9fc49e312fc268fb04eec548fe
João Pedro Pagno <pagno.jp@gmail.com>
2019-10-12 22:15:05 -0300

    Updates

---
+++ precache-manifest.532c9c0df2195761839094ffce8e2d6e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dfe010ba9d19ad7eb0fb85e8706478b",
+    "url": "/pagnojp/redux-saga-spotify/index.html"
+  },
+  {
+    "revision": "18bd9f92b9ab9c4e78a8",
+    "url": "/pagnojp/redux-saga-spotify/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "18bd9f92b9ab9c4e78a8",
+    "url": "/pagnojp/redux-saga-spotify/static/js/2.cece9e52.chunk.js"
+  },
+  {
+    "revision": "149333a725b85845fd78",
+    "url": "/pagnojp/redux-saga-spotify/static/js/main.6302faf2.chunk.js"
+  },
+  {
+    "revision": "a9df6721767b8c452897",
+    "url": "/pagnojp/redux-saga-spotify/static/js/runtime-main.34814933.js"
+  },
+  {
+    "revision": "3f6e21d5b7262e386bc99db029dca18c",
+    "url": "/pagnojp/redux-saga-spotify/static/media/add_playlist.3f6e21d5.svg"
+  },
+  {
+    "revision": "198550c154cb26037ac4ec6b3a1fd664",
+    "url": "/pagnojp/redux-saga-spotify/static/media/backward.198550c1.svg"
+  },
+  {
+    "revision": "52df934abf9666bb18f978501f03322f",
+    "url": "/pagnojp/redux-saga-spotify/static/media/clock.52df934a.svg"
+  },
+  {
+    "revision": "11413de0b991f38fad8fcba58669f194",
+    "url": "/pagnojp/redux-saga-spotify/static/media/close.11413de0.svg"
+  },
+  {
+    "revision": "63fca2afee55e0fcfd8975b787a3908f",
+    "url": "/pagnojp/redux-saga-spotify/static/media/forward.63fca2af.svg"
+  },
+  {
+    "revision": "3fd45499efe15cfa5ac26000494ec532",
+    "url": "/pagnojp/redux-saga-spotify/static/media/loading.3fd45499.svg"
+  },
+  {
+    "revision": "ee8bd067458e4da29de46501efe95dee",
+    "url": "/pagnojp/redux-saga-spotify/static/media/pause.ee8bd067.svg"
+  },
+  {
+    "revision": "6557e51e1e6d913c5a89e62b2791cdc2",
+    "url": "/pagnojp/redux-saga-spotify/static/media/play.6557e51e.svg"
+  },
+  {
+    "revision": "38b523e46c97e55c30ac3169007b0d4a",
+    "url": "/pagnojp/redux-saga-spotify/static/media/plus.38b523e4.svg"
+  },
+  {
+    "revision": "716dfa0b41f0008fced2daef09f22aeb",
+    "url": "/pagnojp/redux-saga-spotify/static/media/repeat.716dfa0b.svg"
+  },
+  {
+    "revision": "75bed367be0218560786fa10bb6f37e4",
+    "url": "/pagnojp/redux-saga-spotify/static/media/search.75bed367.svg"
+  },
+  {
+    "revision": "54850ef47ae6d4d038552c89fafeb57b",
+    "url": "/pagnojp/redux-saga-spotify/static/media/shuffle.54850ef4.svg"
+  },
+  {
+    "revision": "6b27f5927a792a595f190dda190747d6",
+    "url": "/pagnojp/redux-saga-spotify/static/media/volume.6b27f592.svg"
+  }
+]);
